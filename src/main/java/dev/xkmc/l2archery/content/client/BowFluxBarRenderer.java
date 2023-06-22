aa071ece468079db917ca2b01069739e0d362c41
lcy0x1 <arthurlcy0x1@gmail.com>
2023-06-22 01:56:35 -0500

    fix flux render

--- src/main/java/dev/xkmc/l2archery/content/client/BowFluxBarRenderer.java
+++ src/main/java/dev/xkmc/l2archery/content/client/BowFluxBarRenderer.java
@@ -1,40 +1,23 @@
 package dev.xkmc.l2archery.content.client;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.*;
 import dev.xkmc.l2archery.content.energy.IFluxItem;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.IItemDecorator;
 
+import static dev.xkmc.l2library.base.overlay.ItemSelSideBar.color;
+
 public class BowFluxBarRenderer implements IItemDecorator {
 
-	public boolean render(PoseStack poseStack, Font font, ItemStack stack, int x, int y) {
+	public boolean render(GuiGraphics g, Font font, ItemStack stack, int x, int y) {
 		IFluxItem item = (IFluxItem) stack.getItem();
 		if (item.getFluxFeature(stack) == null) return false;
-		Tesselator t = Tesselator.getInstance();
-		BufferBuilder builder = t.getBuilder();
 		int w = Mth.ceil(13.0F * item.getEnergyStored(stack) / item.getMaxEnergyStored(stack));
-		fillRect(builder, x + 2, y + 14, w, 1, 0, 255, 255, 255, 255);
-		fillRect(builder, x + 2 + w, y + 14, 13 - w, 1, 0, 0, 0, 0, 255);
+		g.fill(x + 2, y + 14, w, 1, color(255, 255, 255, 255));
+		g.fill(x + 2 + w, y + 14, 13 - w, 1, 0, color(0, 0, 0, 255));
 		return true;
 	}
-
-	/**
-	 * Draw with the WorldRenderer
-	 */
-	private static void fillRect(BufferBuilder buffer, int x, int y, int w, int h, int z, int r, int g, int b, int a) {
-		RenderSystem.disableDepthTest();
-		RenderSystem.setShader(GameRenderer::getPositionColorShader);
-		buffer.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
-		buffer.vertex(x, y, z).color(r, g, b, a).endVertex();
-		buffer.vertex(x, y + h, z).color(r, g, b, a).endVertex();
-		buffer.vertex(x + w, y + h, z).color(r, g, b, a).endVertex();
-		buffer.vertex(x + w, y, z).color(r, g, b, a).endVertex();
-		BufferUploader.drawWithShader(buffer.end());
-	}
-
 
 }
